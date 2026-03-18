c3e176e9651b67e4ea5f1b7869f190a0e72b2541
EduardoADC <67719815+EduardoADC@users.noreply.github.com>
2026-03-18 13:01:05 -0500

    Initial commit

---
+++ routes/subscription.routes.js
@@ -0,0 +1,35 @@
+import { Router } from "express";
+import authorize from "../middlewares/auth.middleware.js";
+import {
+  createSubscription,
+  getUserSubscriptions,
+} from "../controllers/subscription.controller.js";
+
+const subscriptionRouter = Router();
+
+subscriptionRouter.get("/", (req, res) =>
+  res.send({ title: "Get all subscriptions" }),
+);
+
+subscriptionRouter.get("/:id", (req, res) =>
+  res.send({ title: "Get subscription details" }),
+);
+
+subscriptionRouter.post("/", authorize, createSubscription);
+
+subscriptionRouter.put("/:id", (req, res) =>
+  res.send({ title: "Update subscription by id" }),
+);
+subscriptionRouter.delete("/:id", (req, res) =>
+  res.send({ title: "Delete a subscription" }),
+);
+subscriptionRouter.get("/user/:id", authorize, getUserSubscriptions);
+
+subscriptionRouter.put("/:id/cancel", (req, res) =>
+  res.send({ title: "Cancel subscription by id" }),
+);
+subscriptionRouter.get("/upcoming-renewals", (req, res) =>
+  res.send({ title: "Get all subscriptions with upcoming renewals" }),
+);
+
+export default subscriptionRouter;
---
+++ routes/user.routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { getUser, getUsers } from "../controllers/user.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
+
+const userRouter = Router();
+
+userRouter.get("/", authorize, getUsers);
+userRouter.get("/:id", authorize, getUser);
+userRouter.post("/", (req, res) => res.send({ title: "Create user" }));
+userRouter.put("/:id", (req, res) => res.send({ title: "Update user by id" }));
+userRouter.delete("/:id", (req, res) =>
+  res.send({ title: "Delete user by id" }),
+);
+
+export default userRouter;
